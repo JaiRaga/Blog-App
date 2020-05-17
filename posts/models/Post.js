170c80a1dc3bdb1ae6e3cccb6d29804e041742cb
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-17 19:55:34 +0530

    created post schema

---
+++ posts/models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    body: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Post = mongoose.model("post", postSchema);
+
+module.exports = Post;
