2d79c68be9905165b48cb395850e3ca88d8b92a8
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-17 19:34:28 +0530

    created db using mongods atlas

---
+++ posts/db/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const db = require("../config/keys").mongoUri;
+
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("Connected to Atlas"))
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
--- posts/index.js
+++ posts/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("./db/mongoose");
 const app = express();
 
 app.listen(9008, () => console.log("Posts on 9008"));
