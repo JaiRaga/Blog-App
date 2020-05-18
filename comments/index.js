05dab5f3b915c94968468f72d557a683beb4e6b6
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-18 19:37:33 +0530

    added cors package and comment router to the express middleware

--- comments/index.js
+++ comments/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
+const commentRouter = require("./routes/comment");
+
 const app = express();
 
-app.listen(9007, () => console.log("Comments on 9008"));
+app.use(express.json());
+app.use(cors());
+app.use("/api", commentRouter);
+
+app.listen(9007, () => console.log("Comments on 9007"));
