1f098a0f2a3c81dccd54451486861cf6aa6a8dd2
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-18 19:34:11 +0530

    added cors middleware and post router

--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
+const postRouter = require("./routes/post");
+
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api", postRouter);
 
 app.listen(9008, () => console.log("Posts on 9008"));
