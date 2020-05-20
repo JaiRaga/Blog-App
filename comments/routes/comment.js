c54b26f9bc7b17f1c9fbe38bc6d5b62bc6145ef9
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-20 19:47:43 +0530

    add events route

--- comments/routes/comment.js
+++ comments/routes/comment.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const Comment = require("../models/Comment");
 const { Router } = express;
 const router = new Router();
@@ -9,6 +10,10 @@ router.post("/posts/:id/comments", async (req, res) => {
 
   try {
     await comment.save();
+    await axios.post("http://localhost:9090/events", {
+      type: "CommentCreated",
+      comment
+    });
     res.status(201).send(comment);
   } catch (err) {
     res.status(400).send(err);
@@ -26,6 +31,17 @@ router.get("/posts/:id/comments", async (req, res) => {
     res.status(200).send(comments);
   } catch (err) {
     res.status(500).send(err);
+  }
+});
+
+router.post("/events", (req, res) => {
+  const event = req.body;
+  try {
+    console.log(event.type);
+    res.send({});
+  } catch (err) {
+    console.log(err);
+    res.send(err);
   }
 });
 
