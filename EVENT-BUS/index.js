fc41174ce150194513b22ee850731fb668b7926c
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-20 19:49:51 +0530

    refactored code

--- EVENT-BUS/index.js
+++ EVENT-BUS/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const axios = require("axios");
+const cors = require("cors");
+const eventRouter = require("./routes/events");
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+app.use(eventRouter);
 
 app.listen(9090, () => console.log("Event Bus on port 9090"));
